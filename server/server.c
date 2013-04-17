7cdc4a876dd5e76b39a6450dc0576324032a499b
Dung Nguyen <clemenation@gmail.com>
2013-04-17 17:32:55 +0700

    Code organise

--- server/server.c
+++ server/server.c
@@ -13,6 +13,75 @@
 #include "protocol.h"
 #include "checksum.h"
 
+void receiveFile(int client, Message writeMsg);
+void handler(int client);
+
+int main(int argc, char *argv[]) {
+    Server *server = create_server();
+
+    server->port = PORT;
+    server->handler = &handler;
+    server_start(server);
+
+    return 0;
+}
+
+void receiveFile(int client, Message writeMsg)
+{
+  int outputFile = open(writeMsg.writeRequest.fileName, 
+  O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR);
+  if (outputFile == -1)
+  {
+    sendAckMessage(client, 0, &server_error);  // 0 means deny, else means accept
+    server_error("cannot open output file");
+  }
+  sendAckMessage(client, 1, &server_error);  // accept
+  printf("Accept message sent\n");
+
+  int id = 0;
+  while (1)
+  {
+    Message *dataMsg;
+
+    dataMsg = receiveMessage(client, MSG_DAT, "receive data packet", &server_error);
+    if (!dataMsg)
+    { // we're done receiving the file
+      break;
+    }
+
+    int bytes_written = Write(outputFile, 
+      dataMsg->dataPacket.data, 
+      dataMsg->dataPacket.size);
+    id = dataMsg->dataPacket.id;
+    if (bytes_written <= 0)
+    { // handle errors
+      server_error("write to output file");
+    }
+
+    free(dataMsg);  // free after use
+  }
+
+  close(outputFile);
+  printf("Finished receiving %s\n", writeMsg.writeRequest.fileName);
+  printf("%d bytes received\n", writeMsg.writeRequest.fileSize);
+  printf("%d packet received\n", id+1);  
+
+  // md5 matching
+  unsigned char *md5 = MD5Checksum(writeMsg.writeRequest.fileName);
+  if (memcmp(md5, writeMsg.writeRequest.md5, MD5_DIGEST_LENGTH) == 0)
+  {
+    printf("MD5 match");
+  }
+  else
+  {
+    printf("ERROR: MD5 mismatch");
+  }
+  printf("(");
+  printMD5Checksum(md5);
+  printf(")\n");
+  free(md5);
+}
+
 void handler(int client) {
   Message writeMsg;
 
@@ -30,72 +99,11 @@ void handler(int client) {
 
   if (choice == 'y')
   {
-    int outputFile = open(writeMsg.writeRequest.fileName, 
-    O_WRONLY | O_CREAT, S_IWUSR | S_IRUSR);
-    if (outputFile == -1)
-    {
-      sendAckMessage(client, 0, &server_error);  // 0 means deny, else means accept
-      server_error("cannot open output file");
-    }
-    sendAckMessage(client, 1, &server_error);  // accept
-    printf("Accept message sent\n");
-
-    int id = 0;
-    while (1)
-    {
-      Message *dataMsg;
-
-      dataMsg = receiveMessage(client, MSG_DAT, "receive data packet", &server_error);
-      if (!dataMsg)
-      { // we're done receiving the file
-        break;
-      }
-
-      int bytes_written = Write(outputFile, 
-        dataMsg->dataPacket.data, 
-        dataMsg->dataPacket.size);
-      id = dataMsg->dataPacket.id;
-      if (bytes_written <= 0)
-      { // handle errors
-        server_error("write to output file");
-      }
-
-      free(dataMsg);  // free after use
-    }
-
-    close(outputFile);
-    printf("Finished receiving %s\n", writeMsg.writeRequest.fileName);
-    printf("%d bytes received\n", writeMsg.writeRequest.fileSize);
-    printf("%d packet received\n", id+1);  
-
-    // md5 matching
-    unsigned char *md5 = MD5Checksum(writeMsg.writeRequest.fileName);
-    if (memcmp(md5, writeMsg.writeRequest.md5, MD5_DIGEST_LENGTH) == 0)
-    {
-      printf("MD5 match");
-    }
-    else
-    {
-      printf("ERROR: MD5 mismatch");
-    }
-    printf("(");
-    printMD5Checksum(md5);
-    printf(")\n");
-    free(md5);
+    receiveFile(client, writeMsg);
   }
   else
   {
     sendAckMessage(client, 0, &server_error);   // deny
     printf("Deny message sent\n");
   }
-}
-
-int main(int argc, char *argv[]) {
-    Server *server = create_server();
-
-    server->port = PORT;
-    server->handler = &handler;
-    server_start(server);
-
-    return 0;
 }
